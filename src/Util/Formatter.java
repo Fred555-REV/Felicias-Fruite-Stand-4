ff8535c20c40c9968e96863c71ce8a62f802b957
Freddy Canido <freddy.canido12@gmail.com>
2021-08-31 17:52:24 -0400

    added a test to find out that exception is not abstract

--- src/Util/Formatter.java
+++ src/Util/Formatter.java
@@ -1,9 +1,27 @@
 package Util;
 
 import java.text.NumberFormat;
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class Formatter {
     public static String getDisplayPrice(int price) {
         return NumberFormat.getCurrencyInstance().format(price / 100.0);
+    }
+
+    public static void test() {
+        Scanner scan = new Scanner(System.in);
+        try {
+            scan.nextInt();
+        } catch (InputMismatchException inputMismatchException) {
+            System.out.println(3);
+        }
+        try {
+            scan.nextInt();
+        } catch (Exception exception) {
+            Exception exception1 = new Exception("Nice",exception);
+            System.out.println(exception1);
+            System.out.println(2);
+        }
     }
 }
--- src/fruit/stand/Test.java
+++ src/fruit/stand/Test.java
@@ -1,5 +1,6 @@
 package fruit.stand;
 
+import Util.Formatter;
 import fruit.stand.products.Fruit;
 
 import java.time.Instant;
@@ -8,8 +9,7 @@ import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
-//        Fruit fruit = new Fruit("apple", "sub", LocalDate.now(), 1, 1, "red");
-//        System.out.println(fruit);
-//        System.out.println(fruit.getAmount());
+        Formatter.test();
+
     }
 }
