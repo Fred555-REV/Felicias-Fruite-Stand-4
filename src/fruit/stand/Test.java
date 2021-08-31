0687d7353fd093272f471d183e96eeed30985e48
Freddy Canido <freddy.canido12@gmail.com>
2021-08-31 19:54:28 -0400

    refactored prob going to transfer formatter and UI to different repo

--- src/fruit/stand/Test.java
+++ src/fruit/stand/Test.java
@@ -1,6 +1,7 @@
 package fruit.stand;
 
 import Util.Formatter;
+import fruit.stand.company.console.UI;
 import fruit.stand.products.Fruit;
 
 import java.time.Instant;
@@ -9,6 +10,7 @@ import java.util.Date;
 
 public class Test {
     public static void main(String[] args) {
+        UI.getInt("number between 1-5",1,5);
         Formatter.test();
 
     }
