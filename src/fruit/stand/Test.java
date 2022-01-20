9deed15ff2df70c0d1fc734f97c2913970b05f54
Freddy Canido <freddy.canido12@gmail.com>
2022-01-20 12:00:32 -0500

    removed obsolete code

--- src/fruit/stand/Test.java
+++ src/fruit/stand/Test.java
@@ -1,7 +1,6 @@
 package fruit.stand;
 
 import Util.Formatter;
-import fruit.stand.company.console.UI;
 import fruit.stand.products.Fruit;
 
 import java.time.Instant;
@@ -9,9 +8,5 @@ import java.time.LocalDate;
 import java.util.Date;
 
 public class Test {
-    public static void main(String[] args) {
-        UI.getInt("number between 1-5",1,5);
-        Formatter.test();
 
-    }
 }
